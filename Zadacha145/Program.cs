8e8adbf8a2a26dce7f497494988aa5ff9a003add
timur <yusupov-rr22@mail.ru>
2022-11-14 23:04:45 +0700

    Выбор из массива строк

---
+++ Zadacha145/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+// заполнение
+Console.WriteLine("Введите размерность массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[n];
+
+
+for(int i = 0; i < n; i ++)
+{
+Console.WriteLine("Введите значение массива:  ");
+array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("[" + string.Join("," , array) + "] ->");
+
+
+for(int j = 0; j < n; j++)
+{
+  if (array[j].Length < 4)
+  {
+       Console.Write("[" +  string.Join("," ,array[j]) + "]");
+  }
+}
